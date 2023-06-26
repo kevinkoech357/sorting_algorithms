6ea4bf49310b15578a17e7556b001cea8ffc4b60
Kevin Koech <kevinkoech@proton.me>
2023-06-26 22:01:05 +0300

    add selection sort algorithm

---
+++ 2-selection_sort.c
@@ -0,0 +1,45 @@
+#include <stddef.h>
+#include "sort.h"
+
+/**
+ * selection_sort - sorts an array using
+ * selection sort algorithm
+ *
+ * @array: pointer to array to be sorted
+ *
+ * @size: size of array
+ *
+ * Return: void
+ *
+ */
+
+void selection_sort(int *array, size_t size)
+{
+	size_t i, j = 0;
+	size_t min;
+	int temp;
+
+	if (array == NULL)
+		return;
+
+	for (i = 0; i < size - 1; i++)
+	{
+		min = i;
+
+		for (j = i + 1; j < size; j++)
+		{
+			if (array[j] < array[min])
+			{
+				min = j;
+			}
+		}
+
+		if (min != i)
+		{
+			temp = array[i];
+			array[i] = array[min];
+			array[min] = temp;
+			print_array(array, size);
+		}
+	}
+}
