890657c631286fd5c5ec8682eee636b5a4aa1ba6
Kevin Koech <kevinkoech@proton.me>
2023-06-27 09:43:35 +0300

    add function prototypes for quick sort and insertion sort algorithms

--- sort.h
+++ sort.h
@@ -22,6 +22,7 @@ void print_list(const listint_t *list);
 void bubble_sort(int *array, size_t size);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
+void insertion_sort_list(listint_t **list);
 
 #endif /* SORT_H */
 
